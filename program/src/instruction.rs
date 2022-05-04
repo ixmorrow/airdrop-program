2202cf59423cc3b050b3bb2207e0ae3473d5d81e
ixmorrow <ivanmorrow14@gmail.com>
2022-05-04 00:28:07 -0500

    clean up code

--- program/src/instruction.rs
+++ program/src/instruction.rs
@@ -1,7 +1,7 @@
 use borsh::{BorshDeserialize, BorshSerialize};
 use shank::ShankInstruction;
 use crate::error::AirdropError::{InstructionUnpackError};
-use solana_program::{program_error::ProgramError, msg, borsh::try_from_slice_unchecked};
+use solana_program::{program_error::ProgramError, msg};
 
 
 #[derive(BorshDeserialize, Debug)]
